fcedb142a66dc7fae687d1e1530c0f182d0400cf
TP2002 <tamaspeti3451@gmail.com>
2020-01-17 15:16:31 +0100

    file2
    file2

---
+++ tpgame másolata/játék.py
@@ -0,0 +1,85 @@
+import pygame
+
+pygame.init()
+WINDOW_SIZE = (700,500)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+display = pygame.Surface((300,200))
+pygame.display.set_caption("First Game")
+player = pygame.image.load('player.png')
+
+vel = 5
+
+game_map = [['2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','2','2','2','2','2','0','0','0','0','0','0','2'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['2','2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','2','2'],
+            ['1','1','2','2','2','2','2','2','2','2','2','2','2','2','2','2','2','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
+
+grass_img = pygame.image.load('grass.png')
+dirt_img = pygame.image.load('dirt.png')
+
+isJump = False
+jumpCount = 10
+
+run = True
+while run:
+    #pygame.time.delay(25)
+    
+    display.fill((255,0,0)) 
+
+    y = 0
+    for layer in game_map:
+        x = 0
+        for tile in layer:
+            if tile == '1':
+                display.blit(dirt_img,(x*16,y*16))
+            if tile == '2':
+                display.blit(grass_img,(x*16,y*16))
+            x += 1
+        y += 1
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > -5:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < 937:
+        x += vel
+    if not(isJump):
+        if keys[pygame.K_DOWN] and y < 425:
+            y += vel   
+        if keys[pygame.K_UP]: 
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.3 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+    
+    #screen.blit(levego, (0,0))
+    #screen.blit(viz, (0, 500))
+    #screen.blit(homok, (0, 500))
+    #screen.blit(player, (x, y))
+    screen.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
+    screen.blit(player, (x, y))
+    pygame.display.update()
+
+    
+
+pygame.quit()
